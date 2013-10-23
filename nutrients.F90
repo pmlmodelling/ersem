ab48bb62303ba560239e4e3592a56c6269b6a03a
Jorn Bruggeman <jorn@bolding-burchard.com>
2013-10-23 20:29:48 +0100

    fixed ersem/nutrient model and use it in python test case

--- nutrients.F90
+++ nutrients.F90
@@ -1,9 +1,8 @@
 #include "fabm_driver.h"
-#define IRON
-module pml_ersem_nutrients
+
+module pml_ersem_nutrient
 
    use fabm_types
-   use fabm_driver
 
    implicit none
 
@@ -11,24 +10,15 @@ module pml_ersem_nutrients
    private
 !
 ! !PUBLIC MEMBER FUNCTIONS:
-   public type_pml_ersem_nutrients
-!
-! !PRIVATE DATA MEMBERS:
-   real(rk), parameter :: secs_pr_day = 86400.0_rk
+   public type_pml_ersem_nutrient
 !
 ! !REVISION HISTORY:!
 !  Original author(s): Jorn Bruggeman
 
-   type,extends(type_base_model) :: type_pml_ersem_nutrients
-!     Variable identifiers
-      type (type_state_variable_id)      :: id_N1p,id_N3n,id_N4n,id_N5s
-#ifdef IRON   
-      type (type_state_variable_id)      :: id_N7f
-#endif
+   type,extends(type_base_model) :: type_pml_ersem_nutrient
+      type (type_state_variable_id) :: id_N
    contains
-   
       procedure :: initialize
-   
    end type
       
 contains
@@ -40,22 +30,20 @@ contains
 !  by the model are registered with FABM.
 !
 ! !INPUT PARAMETERS:
-   class (type_pml_ersem_nutrients), intent(inout), target :: self
-   integer,                          intent(in)            :: configunit
+   class (type_pml_ersem_nutrient), intent(inout), target :: self
+   integer,                         intent(in)            :: configunit
 !
 ! !REVISION HISTORY:
 !
 ! !LOCAL VARIABLES:
+   character(len=1024) :: name,units,long_name
 !EOP
 !-----------------------------------------------------------------------
 !BOC
-   call self%register_state_variable(self%id_N1p,'N1p','mmol P/m^3','Phosphate', 1._rk,minimum=0._rk)
-   call self%register_state_variable(self%id_N3n,'N3n','mmol N/m^3','Nitrate',   1._rk,minimum=0._rk)
-   call self%register_state_variable(self%id_N4n,'N4n','mmol N/m^3','Ammonium',  1._rk,minimum=0._rk)
-   call self%register_state_variable(self%id_N5s,'N5s','mmol S/m^3','Silicate',  1._rk,minimum=0._rk)
-#ifdef IRON   
-   call self%register_state_variable(self%id_N7f,'N7f','umol F/m^3','Inorganic Iron', 1._rk, minimum=0._rk)
-#endif
+   call self%get_parameter(name,'name')
+   call self%get_parameter(units,'units')
+   call self%get_parameter(long_name,'long_name',default=name)
+   call self%register_state_variable(self%id_N,name,units,long_name,1._rk,minimum=0._rk)
 
    end subroutine
 
