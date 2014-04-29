c1157f5a3a5bbc5df8e4b9726fa0af988470cdbd
Jorn Bruggeman <jbr@pml.ac.uk>
2014-04-29 12:43:37 +0100

    cleaned-up benthic pom

--- benthic_pom.F90
+++ benthic_pom.F90
@@ -1,5 +1,8 @@
 #include "fabm_driver.h"
 
+! Benthic variable that supports resuspension and remineralization.
+! Both processes return material to the pelagic.
+
 module pml_ersem_benthic_pom
 
    use fabm_types
@@ -117,122 +120,128 @@ contains
       real(rk) :: bedsedXc,bedsedXn,bedsedXp,bedsedXs
       real(rk) :: fac,FerC,FerN,FerP,FerS,FerF
 
-      if (self%resuspension) then
-         _HORIZONTAL_LOOP_BEGIN_
-
-   !     Bed characteristics - from Puls and Sundermann 1990
-   !     Critical shear velocity for erosion = 0.02 m/s
-   !
-         ter=0.02_rk**2
-   !
-   !     erosion constant (g/m^2/s)
-   !
-         er=100._rk*Ter * 1000._rk*86400._rk  !convert to mg/day
-
-         _GET_HORIZONTAL_(self%id_bedstress,bedstress)
-         _GET_HORIZONTAL_(self%id_wdepth,wdepth)
-         _GET_HORIZONTAL_(self%id_c,Q6cP)
-         _GET_HORIZONTAL_(self%id_n,Q6nP)
-         _GET_HORIZONTAL_(self%id_p,Q6pP)
-         _GET_(self%id_dens,density)
-
-         ! Divide actual stress (Pa) by density (kg m-3) to obtain square of bed shear velocity.
-         bedstress = bedstress/density
-   !
-         if(bedstress.gt.ter) then
-   !     Inorganic sedment (could replace by transport model)
-   !     for now assume 90% is a fixed inorganic component
-         bedsedXc=10._rk*(-1069._rk*LOG(wdepth) + 10900._rk)
-         bedsedXn=10._rk*(-7.6368_rk*LOG(wdepth) + 78.564_rk)
-         bedsedXp=10._rk*(-0.545_rk*LOG(wdepth) + 6.0114_rk)
-         bedsedXs=10._rk*(-64.598_rk*LOG(wdepth) + 391.61_rk)
-   !      fac = er*(bedstress(k)/ter - 1.0)
-         fac = er*(bedstress/ter - 1._rk)/(Q6cP+bedsedXc)
-   ! C
-   !      FerC=fac*Q6cP(k)/(Q6cP(k)+bedsedXc)
-         FerC=fac*Q6cP
-         !FerC=max(min(FerC,Q6cP(k)/timestep+ &
-         !       min(SQ6c(k)-wsoQ6c(k),0._rk)),0._rk)
-   ! N
-   !      FerN=fac*Q6nP(k)/(Q6nP(k)+bedsedXn)
-         FerN=fac*Q6nP
-         !FerN=max(min(FerN,Q6nP(k)/timestep+ &
-         !       min(SQ6n(k)-wsoQ6n(k),0._rk)),0._rk)
-   ! P
-   !      FerP=fac*Q6pP(k)/(Q6pP(k)+bedsedXp)
-         FerP=fac*Q6pP
-         !FerP=max(min(FerP,Q6pP(k)/timestep+ &
-         !       min(SQ6p(k)-wsoQ6p(k),0._rk)),0._rk)
-         if (_VARIABLE_REGISTERED_(self%id_s)) then
-   ! S
-   !      FerS=fac*Q6sP(k)/(Q6sP(k)+bedsedXs)
-         _GET_HORIZONTAL_(self%id_s,Q6sP)
-         FerS=fac*Q6sP
-         !FerS=max(min(FerS,Q6sP(k)/timestep+ &
-         !       min(SQ6s(k)-wsoQ6s(k),0._rk)),0._rk)
-         _SET_ODE_BEN_(self%id_s,-FerS)
-         _SET_BOTTOM_EXCHANGE_(self%id_resuspension_s,FerS)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_f)) then
-   ! F
-         _GET_HORIZONTAL_(self%id_f,Q6fP)
-         FerF=fac*Q6fP
-         !FerF=max(min(FerF,Q6fP(k)/timestep+ &
-         !       min(SQ6f(k)-wsoQ6f(k),0._rk)),0._rk)
-         _SET_ODE_BEN_(self%id_f,-FerF)
-         _SET_BOTTOM_EXCHANGE_(self%id_resuspension_f,FerF)
-         end if
-
-         _SET_ODE_BEN_(self%id_c,-FerC)
-         _SET_ODE_BEN_(self%id_n,-FerN)
-         _SET_ODE_BEN_(self%id_p,-FerP)
-
-         _SET_BOTTOM_EXCHANGE_(self%id_resuspension_c,FerC)
-         _SET_BOTTOM_EXCHANGE_(self%id_resuspension_n,FerN)
-         _SET_BOTTOM_EXCHANGE_(self%id_resuspension_p,FerP)
-
-         endif
-   ! end of resuspension bit
-      _HORIZONTAL_LOOP_END_
-   end if
-      
-   ! Remineralization (benthic return)
-   if (self%reminQIX/=0.0_rk) then
       _HORIZONTAL_LOOP_BEGIN_
-         if (_VARIABLE_REGISTERED_(self%id_c)) then
-            _GET_HORIZONTAL_(self%id_c,Q6cP)
-            _SET_ODE_BEN_(self%id_c,-self%reminQIX*Q6cP)
-            _SET_BOTTOM_EXCHANGE_(self%id_O3c,self%reminQIX*Q6cP/CMass)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_p)) then
-            _GET_HORIZONTAL_(self%id_p,Q6pP)
-            _SET_ODE_BEN_(self%id_p,-self%reminQIX*Q6pP)
-            _SET_BOTTOM_EXCHANGE_(self%id_N1p,self%reminQIX*Q6pP)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_n)) then
-            _GET_HORIZONTAL_(self%id_n,Q6nP)
-            _SET_ODE_BEN_(self%id_n,-self%reminQIX*Q6nP)
-            _SET_BOTTOM_EXCHANGE_(self%id_N3n,self%pQIN3X*self%reminQIX*Q6nP)
-            _SET_BOTTOM_EXCHANGE_(self%id_N4n,(1.0_rk-self%pQIN3X)*self%reminQIX*Q6nP)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_s)) then
-            _GET_HORIZONTAL_(self%id_s,Q6sP)
-            _SET_ODE_BEN_(self%id_s,-self%reminQIX*Q6sP)
-            _SET_BOTTOM_EXCHANGE_(self%id_N5s,self%reminQIX*Q6sP)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_f)) then
-            _GET_HORIZONTAL_(self%id_f,Q6fP)
-            _SET_ODE_BEN_(self%id_f,-self%reminQIX*Q6fP)
-            _SET_BOTTOM_EXCHANGE_(self%id_N7f,self%reminQIX*Q6fP)
-         end if
-         if (_VARIABLE_REGISTERED_(self%id_l)) then
-            _GET_HORIZONTAL_(self%id_l,Q6lP)
-            _SET_ODE_BEN_(self%id_l,-self%reminQIX*Q6lP)
-            _SET_BOTTOM_EXCHANGE_(self%id_O3c,self%reminQIX*Q6lP/CMass)
-         end if
-      _HORIZONTAL_LOOP_END_
-   end if
+         if (self%resuspension) then
+            ! Bed characteristics - from Puls and Sundermann 1990
+            ! Critical shear velocity for erosion = 0.02 m/s
+            ter=0.02_rk**2
 
+            _GET_HORIZONTAL_(self%id_bedstress,bedstress)
+            _GET_(self%id_dens,density)
+
+            ! Divide actual stress (Pa) by density (kg m-3) to obtain square of bed shear velocity.
+            ! This allows for comparison with ter.
+            bedstress = bedstress/density
+         else
+            bedstress = 0.0_rk
+         end if
+
+         if (bedstress.gt.ter) then
+            _GET_HORIZONTAL_(self%id_wdepth,wdepth)
+            _GET_HORIZONTAL_(self%id_c,Q6cP)
+
+            ! erosion constant (g/m^2/s)
+            er=100._rk*Ter * 1000._rk*86400._rk  !convert to mg/day
+
+            ! Inorganic sedment (could replace by transport model)
+            ! for now assume 90% is a fixed inorganic component
+            bedsedXc=10._rk*(-1069._rk*LOG(wdepth) + 10900._rk)
+            bedsedXn=10._rk*(-7.6368_rk*LOG(wdepth) + 78.564_rk)
+            bedsedXp=10._rk*(-0.545_rk*LOG(wdepth) + 6.0114_rk)
+            bedsedXs=10._rk*(-64.598_rk*LOG(wdepth) + 391.61_rk)
+
+            ! fac = er*(bedstress(k)/ter - 1.0)
+            fac = er*(bedstress/ter - 1._rk)/(Q6cP+bedsedXc)
+
+            ! Carbon
+            ! FerC=fac*Q6cP(k)/(Q6cP(k)+bedsedXc)
+            FerC=fac*Q6cP
+            !FerC=max(min(FerC,Q6cP(k)/timestep+ &
+            !       min(SQ6c(k)-wsoQ6c(k),0._rk)),0._rk)
+            _SET_ODE_BEN_(self%id_c,-FerC)
+            _SET_BOTTOM_EXCHANGE_(self%id_resuspension_c,FerC)
+            
+            ! Nitrogen
+            if (_VARIABLE_REGISTERED_(self%id_n)) then
+               _GET_HORIZONTAL_(self%id_n,Q6nP)
+               ! FerN=fac*Q6nP(k)/(Q6nP(k)+bedsedXn)
+               FerN=fac*Q6nP
+               !FerN=max(min(FerN,Q6nP(k)/timestep+ &
+               !       min(SQ6n(k)-wsoQ6n(k),0._rk)),0._rk)
+               _SET_ODE_BEN_(self%id_n,-FerN)
+               _SET_BOTTOM_EXCHANGE_(self%id_resuspension_n,FerN)
+            end if
+
+            ! Phosphorus
+            if (_VARIABLE_REGISTERED_(self%id_p)) then
+               _GET_HORIZONTAL_(self%id_p,Q6pP)
+               ! FerP=fac*Q6pP(k)/(Q6pP(k)+bedsedXp)
+               FerP=fac*Q6pP
+               !FerP=max(min(FerP,Q6pP(k)/timestep+ &
+               !       min(SQ6p(k)-wsoQ6p(k),0._rk)),0._rk)
+               _SET_ODE_BEN_(self%id_p,-FerP)
+               _SET_BOTTOM_EXCHANGE_(self%id_resuspension_p,FerP)
+            end if
+            
+            ! Silicon
+            if (_VARIABLE_REGISTERED_(self%id_s)) then
+               _GET_HORIZONTAL_(self%id_s,Q6sP)
+               ! FerS=fac*Q6sP(k)/(Q6sP(k)+bedsedXs)
+               FerS=fac*Q6sP
+               !FerS=max(min(FerS,Q6sP(k)/timestep+ &
+               !       min(SQ6s(k)-wsoQ6s(k),0._rk)),0._rk)
+               _SET_ODE_BEN_(self%id_s,-FerS)
+               _SET_BOTTOM_EXCHANGE_(self%id_resuspension_s,FerS)
+            end if
+
+            ! Iron
+            if (_VARIABLE_REGISTERED_(self%id_f)) then
+               _GET_HORIZONTAL_(self%id_f,Q6fP)
+               FerF=fac*Q6fP
+               !FerF=max(min(FerF,Q6fP(k)/timestep+ &
+               !       min(SQ6f(k)-wsoQ6f(k),0._rk)),0._rk)
+               _SET_ODE_BEN_(self%id_f,-FerF)
+               _SET_BOTTOM_EXCHANGE_(self%id_resuspension_f,FerF)
+            end if
+         end if   ! Resuspension
+
+         ! Remineralization (benthic return)
+         if (self%reminQIX/=0.0_rk) then
+            if (_VARIABLE_REGISTERED_(self%id_c)) then
+               _GET_HORIZONTAL_(self%id_c,Q6cP)
+               _SET_ODE_BEN_(self%id_c,-self%reminQIX*Q6cP)
+               _SET_BOTTOM_EXCHANGE_(self%id_O3c,self%reminQIX*Q6cP/CMass)
+            end if
+            if (_VARIABLE_REGISTERED_(self%id_p)) then
+               _GET_HORIZONTAL_(self%id_p,Q6pP)
+               _SET_ODE_BEN_(self%id_p,-self%reminQIX*Q6pP)
+               _SET_BOTTOM_EXCHANGE_(self%id_N1p,self%reminQIX*Q6pP)
+            end if
+            if (_VARIABLE_REGISTERED_(self%id_n)) then
+               _GET_HORIZONTAL_(self%id_n,Q6nP)
+               _SET_ODE_BEN_(self%id_n,-self%reminQIX*Q6nP)
+               _SET_BOTTOM_EXCHANGE_(self%id_N3n,self%pQIN3X*self%reminQIX*Q6nP)
+               _SET_BOTTOM_EXCHANGE_(self%id_N4n,(1.0_rk-self%pQIN3X)*self%reminQIX*Q6nP)
+            end if
+            if (_VARIABLE_REGISTERED_(self%id_s)) then
+               _GET_HORIZONTAL_(self%id_s,Q6sP)
+               _SET_ODE_BEN_(self%id_s,-self%reminQIX*Q6sP)
+               _SET_BOTTOM_EXCHANGE_(self%id_N5s,self%reminQIX*Q6sP)
+            end if
+            if (_VARIABLE_REGISTERED_(self%id_f)) then
+               _GET_HORIZONTAL_(self%id_f,Q6fP)
+               _SET_ODE_BEN_(self%id_f,-self%reminQIX*Q6fP)
+               _SET_BOTTOM_EXCHANGE_(self%id_N7f,self%reminQIX*Q6fP)
+            end if
+            if (_VARIABLE_REGISTERED_(self%id_l)) then
+               _GET_HORIZONTAL_(self%id_l,Q6lP)
+               _SET_ODE_BEN_(self%id_l,-self%reminQIX*Q6lP)
+               _SET_BOTTOM_EXCHANGE_(self%id_O3c,self%reminQIX*Q6lP/CMass)
+            end if
+         end if   ! Remineralization (benthic return)
+
+      _HORIZONTAL_LOOP_END_
+      
    end subroutine
 
 end module
