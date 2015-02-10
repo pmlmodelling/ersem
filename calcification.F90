6333ace7f58b035324db83bfd9d5e54412c6eb2d
Jorn Bruggeman <jbr@pml.ac.uk>
2015-02-10 16:33:24 +0000

    cleaned up calcification.F90

--- calcification.F90
+++ calcification.F90
@@ -55,7 +55,7 @@ contains
       end select
       call self%get_parameter(self%Rain0,'Rain0','-','maximum rain ratio from PISCES')
       call self%get_parameter(sedL2,'sedL2','m/d','sinking velocity')
-      call self%get_parameter(c0,'c0','mg C m^-3','background concentration',default=0.0_rk)
+      call self%get_parameter(c0,'c0','mg C/m^3','background concentration',default=0.0_rk)
 
       call self%initialize_ersem_base(rm=sedL2,sedimentation=.false.)
       call self%get_parameter(self%sedimentation,'sedimentation','','enable sedimentation',default=.true.)
@@ -64,7 +64,7 @@ contains
       call self%register_diagnostic_variable(self%id_RainR,'RainR','1','rain ratio')
 
       call self%register_dependency(self%id_om_cal,'om_cal','-','calcite saturation')
-      call self%register_state_dependency(self%id_O3c,'O3c','mmol C/m**3','total dissolved inorganic carbon')
+      call self%register_state_dependency(self%id_O3c,'O3c','mmol C/m^3','total dissolved inorganic carbon')
 
       if (self%sedimentation) then
           call self%register_bottom_state_dependency(self%id_bL2c,'bL2c','mg C/m^3','benthic calcite')
@@ -99,15 +99,20 @@ contains
          _SET_ODE_(self%id_O3c, fdiss*L2c/CMass)
          _SET_DIAGNOSTIC_(self%id_RainR,fcalc * self%Rain0)
       _LOOP_END_
-   end subroutine
+
+   end subroutine do
 
    subroutine do_bottom(self,_ARGUMENTS_DO_BOTTOM_)
       class (type_ersem_calcification), intent(in) :: self
       _DECLARE_ARGUMENTS_DO_BOTTOM_
 
       real(rk) :: tbed,density
-      real(rk) :: tdep,fac,sdrate,fsd
+      real(rk) :: fac,sdrate,fsd
       real(rk) :: L2c
+
+      ! Bed characteristics - from Puls and Sundermann 1990
+      ! Critical bed shear velocity = 0.01 m/s
+      real(rk) :: tdep = 0.01_rk**2
 
       if (.not.self%sedimentation) return
 
@@ -122,24 +127,21 @@ contains
 
          ! Divide actual stress (Pa) by density (kg/m^3) to obtain square of bed shear velocity.
          tbed = tbed/density
-!
-!     Bed characteristics - from Puls and Sundermann 1990
-!     Critical bed shear velocity = 0.01 m/s
-!
-      tdep=0.01_rk**2
-!
-     if(tbed .lt. tdep) then
-       fac=1._rk-tbed/tdep
-     else
-       fac=0._rk
-     endif
-!
-      !sdrate = min(fsd*fac,pdepth(I)/timestep) ! Jorn: CFL criterion disabled because FABM does not provide timestep
-      sdrate = fsd*fac
 
-      _SET_BOTTOM_ODE_(self%id_bL2c, sdrate*L2c)
-      _SET_BOTTOM_EXCHANGE_(self%id_c,-sdrate*L2c)
+         if(tbed<tdep) then
+            fac=1._rk-tbed/tdep
+         else
+            fac=0._rk
+         endif
+
+         !sdrate = min(fsd*fac,pdepth(I)/timestep) ! Jorn: CFL criterion disabled because FABM does not provide timestep
+         sdrate = fsd*fac
+
+         _SET_BOTTOM_ODE_(self%id_bL2c, sdrate*L2c)
+         _SET_BOTTOM_EXCHANGE_(self%id_c,-sdrate*L2c)
 
       _HORIZONTAL_LOOP_END_
-   end subroutine
+
+   end subroutine do_bottom
+
 end module
