e12bda06864adf6a31cb0b5a4aee8f1142c50b44
Jorn Bruggeman <jbr@pml.ac.uk>
2015-02-13 16:48:27 +0000

    bacteria cleanup: removed DOCDYN references, reorganized remineralization

--- bacteria.F90
+++ bacteria.F90
@@ -15,9 +15,8 @@ module ersem_bacteria
    type,extends(type_ersem_pelagic_base),public :: type_ersem_bacteria
       ! Variables
       type (type_state_variable_id) :: id_O3c, id_O2o
-      type (type_state_variable_id) :: id_R1c, id_R2c, id_R3c
-      type (type_state_variable_id) :: id_R1p
-      type (type_state_variable_id) :: id_R1n
+      type (type_state_variable_id) :: id_R1c, id_R1p, id_R1n
+      type (type_state_variable_id) :: id_R2c
       type (type_state_variable_id) :: id_N1p,id_N3n,id_N4n,id_N7f
       type (type_dependency_id)     :: id_ETW,id_eO2mO2
       type (type_state_variable_id),allocatable,dimension(:) :: id_RPc,id_RPp,id_RPn,id_RPf
@@ -34,22 +33,14 @@ module ersem_bacteria
       real(rk) :: puB1X,puB1oX,srsB1X
       real(rk) :: qpB1cX,qnB1cX
       real(rk) :: urB1_O2X
-#ifdef DOCDYN
-      real(rk) :: rR2B1X,rR3B1X
-      real(rk),allocatable :: sRPR1(:)
-      real(rk) :: frB1R3
-#else
       real(rk) :: R1R2X
       real(rk),allocatable :: puRP_B1X(:)
-#endif
 
       ! Remineralization
       real(rk) :: sR1N1X,sR1N4X
       real(rk) :: fsinkX
-#ifndef DOCDYN
       real(rk) :: redfieldX
       real(rk) :: rR2R1X
-#endif
    contains
 !     Model procedures
       procedure :: initialize
@@ -66,7 +57,7 @@ contains
 !
 ! !INPUT PARAMETERS:
       class (type_ersem_bacteria),intent(inout),target :: self
-      integer,                        intent(in)           :: configunit
+      integer,                    intent(in)           :: configunit
 !
 ! !REVISION HISTORY:
 !
@@ -95,11 +86,9 @@ contains
       call self%get_parameter(self%sR1N1X,   'sR1N1',   '1/d',    'mineralisation rate of labile dissolved organic phosphorus')
       call self%get_parameter(self%sR1N4X,   'sR1N4',   '1/d',    'mineralisation rate of labile dissolved organic nitrogen')
       call self%get_parameter(self%fsinkX,   'fsink',   '1/d',    'scavenging rate for iron')
-      
-#ifndef DOCDYN
+
       call self%get_parameter(self%redfieldX,'redfield','mol/mol','Redfield carbon to nitrogen ratio')
       call self%get_parameter(self%rR2R1X,   'rR2R1', '1/d', 'specific rate of breakdown of semi-labile to labile DOC')
-#endif
 
       call self%get_parameter(c0,'c0','mg C/m^3','background carbon concentration')
 
@@ -116,12 +105,12 @@ contains
       if (use_iron) call self%register_state_dependency(self%id_N7f,'N7f','umol Fe/m^3','inorganic iron')
 
       ! Register links to labile dissolved organic matter pools.
-      call self%register_state_dependency(self%id_R1c,'R1c','mg C/m^3',  'DOC')
-      call self%register_state_dependency(self%id_R1p,'R1p','mmol P/m^3','DOP')    
-      call self%register_state_dependency(self%id_R1n,'R1n','mmol N/m^3','DON')    
+      call self%register_state_dependency(self%id_R1c,'R1c','mg C/m^3',  'labile dissolved organic carbon')
+      call self%register_state_dependency(self%id_R1p,'R1p','mmol P/m^3','labile dissolved organic phosphorus')    
+      call self%register_state_dependency(self%id_R1n,'R1n','mmol N/m^3','labile dissolved organic nitrogen')    
 
       ! Register links to semi-labile dissolved organic matter pools.
-      call self%register_state_dependency(self%id_R2c,'R2c','mg C/m^3','semi-labile DOC')    
+      call self%register_state_dependency(self%id_R2c,'R2c','mg C/m^3','semi-labile dissolved organic carbon')    
 
       ! Register links to particulate organic matter pools.
       call self%get_parameter(self%nRP,'nRP','','number of substrates',default=0)
@@ -132,33 +121,19 @@ contains
       allocate(self%id_RPf(self%nRP))
       do iRP=1,self%nRP
          write (index,'(i0)') iRP
-         call self%register_state_dependency(self%id_RPc(iRP),'RP'//trim(index)//'c','mg C/m^3',   'POC '//trim(index))    
-         call self%register_state_dependency(self%id_RPn(iRP),'RP'//trim(index)//'n','mmol N/m^3', 'PON '//trim(index))    
-         call self%register_state_dependency(self%id_RPp(iRP),'RP'//trim(index)//'p','mmol P/m^3', 'POP '//trim(index))    
+         call self%register_state_dependency(self%id_RPc(iRP),'RP'//trim(index)//'c','mg C/m^3',   'carbon in substrate '//trim(index))    
+         call self%register_state_dependency(self%id_RPn(iRP),'RP'//trim(index)//'n','mmol N/m^3', 'nitrogen in substrate '//trim(index))    
+         call self%register_state_dependency(self%id_RPp(iRP),'RP'//trim(index)//'p','mmol P/m^3', 'phosphorus in substrate '//trim(index))    
          call self%register_model_dependency(self%id_RP(iRP),'RP'//trim(index))
          call self%request_coupling_to_model(self%id_RPc(iRP),self%id_RP(iRP),'c')
          call self%request_coupling_to_model(self%id_RPn(iRP),self%id_RP(iRP),'n')
          call self%request_coupling_to_model(self%id_RPp(iRP),self%id_RP(iRP),'p')
          if (use_iron) then
-            call self%register_state_dependency(self%id_RPf(iRP),'RP'//trim(index)//'f','umol Fe/m^3','POFe '//trim(index),required=.false.)    
+            call self%register_state_dependency(self%id_RPf(iRP),'RP'//trim(index)//'f','umol Fe/m^3','iron in substrate '//trim(index),required=.false.)    
             call self%request_coupling_to_model(self%id_RPf(iRP),self%id_RP(iRP),'f')
          end if
       end do
-      
-#ifdef DOCDYN
-      ! Register links to semi-refractory dissolved organic matter pool.
-      call self%register_state_dependency(self%id_R3c,'R3c','mg C/m^3','Semi-refractory DOC')
 
-      allocate(self%sRPR1(self%nRP))
-      do iRP=1,self%nRP
-         write (index,'(i0)') iRP
-         call self%get_parameter(self%sRPR1(iRP),'sRP'//trim(index)//'R1','1/d','remineralisation of substrate '//trim(index)//' to DOM')
-      end do
-
-      call self%get_parameter(self%rR2B1X,'rR2','-','fraction of semi-labile DOC available to bacteria')
-      call self%get_parameter(self%rR3B1X,'rR3','-','fraction of semi-refractory DOC available to bacteria')
-      call self%get_parameter(self%frB1R3,'frR3','-','fraction of activity respiration converted to semi-refractory DOC')
-#else
       allocate(self%puRP_B1X(self%nRP))
       do iRP=1,self%nRP
          write (index,'(i0)') iRP
@@ -166,7 +141,6 @@ contains
       end do
 
       call self%get_parameter(self%R1R2X,'R1R2','-','labile fraction of produced dissolved organic carbon')
-#endif
 
       ! Register links to external total dissolved inorganic carbon, dissolved oxygen pools
       call self%register_state_dependency(self%id_O3c,'O3c','mmol C/m^3','carbon dioxide')
@@ -183,7 +157,7 @@ contains
       call self%add_to_aggregate_variable(bacterial_respiration_rate,self%id_fB1O3c)
 
    end subroutine
-   
+
    subroutine do(self,_ARGUMENTS_DO_)
 
       class (type_ersem_bacteria),intent(in) :: self
@@ -201,16 +175,7 @@ contains
       real(rk) :: netb1,bge
       real(rk) :: fB1N1p,fR1B1p,fB1RDp
       real(rk) :: fB1NIn,fR1B1n,fB1RDn
-#ifdef DOCDYN
-      real(rk) :: R3c,R2cP,R3cP
-      real(rk) :: fB1R1c
-      real(rk) :: totsubst
-      integer  :: iRP
-      real(rk),dimension(self%nRP) :: RPc,RPcP,RPnP,RPpP
-      real(rk),dimension(self%nRP) :: fRPB1p,fRPB1n
-#else
       real(rk) :: Nlim,Plim
-#endif
 
       ! Enter spatial loops (if any)
       _LOOP_BEGIN_
@@ -233,17 +198,7 @@ contains
          _GET_(self%id_R1c,R1cP)
          _GET_(self%id_R1p,R1pP)
          _GET_(self%id_R1n,R1nP)
-#ifdef DOCDYN
-         _GET_WITH_BACKGROUND_(self%id_R3c,R3c)
-         _GET_(self%id_R2c,R2cP)
-         _GET_(self%id_R3c,R3cP)
-         do iRP=1,self%nRP
-            _GET_WITH_BACKGROUND_(self%id_RPc(iRP),RPc(iRP))
-            _GET_(self%id_RPc(iRP),RPcP(iRP))
-            _GET_(self%id_RPn(iRP),RPnP(iRP))
-            _GET_(self%id_RPp(iRP),RPpP(iRP))
-         end do
-#endif
+
          qpB1c = B1p/B1c
          qnB1c = B1n/B1c
 !..Temperature effect on pelagic bacteria:
@@ -256,7 +211,6 @@ contains
 
 !..Potential nutrient limitation on bacterial growth after Anderson
 
-#ifndef DOCDYN
          IF (self%iswBlimX .EQ. 1 ) THEN
            Nlim = min(N4nP/(N4nP+self%chB1nX),R1nP/(R1nP+self%chB1nX))
            Plim = min(N1pP/(N1pP+self%chB1pX),R1pP/(R1pP+self%chB1pX))
@@ -264,16 +218,11 @@ contains
            Nlim = (N4nP+R1nP)/(N4nP+R1nP+self%chB1nX)
            Plim = (N1pP+R1pP)/(N1pP+R1pP+self%chB1pX)
          END IF
-#endif
 
 !..bacterial mortality
 
          sB1RD=self%sdB1X*etB1
-#ifdef DOCDYN
-         fB1R1c = sB1RD*B1cP
-#else
          fB1RDc = sB1RD*B1cP
-#endif
 
 !..Total amount of substrate available
 
@@ -281,30 +230,14 @@ contains
          ! rutB1 = sutB1*R1cP(I)
 
 !..Potential uptake :
-#ifdef DOCDYN
-         rumB1 = self%sumB1X*etB1*eO2B1*B1c
-#else
          rumB1 = self%sumB1X*etB1*eO2B1*min(Nlim,Plim)*B1c
-#endif
 !..Actual uptake
 
       ! rugB1 = MIN(rumB1,rutB1)
       ! specific in substrate concentration:
-#ifdef DOCDYN
-         totsubst = R1c+R2c*self%rR2B1X+R3c*self%rR3B1X+sum(RPc*self%sRPR1/sutB1)
-         ! Jorn: check whether total substrate>0 to prevent NaNs
-         if (totsubst>0.0_rk) then
-            sugB1 = rumB1/max(rumB1/sutB1,totsubst)
-         else
-            sugB1 = 0.0_rk
-         end if
-             ! = MIN(rumB1,rutB1)=MIN(rumB1/(R1cP+R2cP*rR2B1X,sutB1) avoid pot. div. by 0
-         rugB1 = sugB1*(R1cP+R2cP*self%rR2B1X+R3c*self%rR3B1X+sum(RPc*self%sRPR1/sutB1))
-#else
          sugB1 = rumB1/max(rumB1/sutB1,R1c)
              ! = MIN(rumB1,rutB1)=MIN(rumB1/R1cP,sutB1) avoid pot. div. by 0
          rugB1 = sugB1*R1cP
-#endif
 
 !..Respiration :
 
@@ -315,15 +248,6 @@ contains
 !      fB1O3c(I) = ( 1._fp8 - puB1X*eO2mO2  - puB1oX*( 1._fp8 - eO2mO2 ) )&
 !                * rugB1  + srsB1X * B1cP(I) * etB1
           _SET_DIAGNOSTIC_(self%id_fB1O3c,fB1O3c)
-
-#ifdef DOCDYN
-! specific release of semilabile DOC 
-! fudge factor 1. as used in Polimene et al. AME 2006
-         sB1R2=max(0._rk,max(1._rk-(qpB1c/self%qpB1cX),1._rk-(qnB1c/self%qnB1cX)))*1._rk
-         fB1R2c=sB1R2*B1cP
-         fB1R3c=self%frB1R3*rraB1
-         fB1RDc = fB1R1c + fB1R2c + fB1R3c
-#endif
 
 !..net bacterial production
 
@@ -338,17 +262,8 @@ contains
 
 
          _SET_ODE_(self%id_c,netb1)
-#ifdef DOCDYN
-         _SET_ODE_(self%id_R1c,+ fB1R1c - sugB1*R1cP)
-         _SET_ODE_(self%id_R2c,+ fB1R2c - sugB1*R2cP*self%rR2B1X)
-         _SET_ODE_(self%id_R3c,+ fB1R3c - sugB1*R3cP*self%rR3B1X)
-         do iRP=1,self%nRP
-            _SET_ODE_(self%id_RPc(iRP),- sugB1*RPcP(iRP)*self%sRPR1(iRP))
-         end do
-#else
          _SET_ODE_(self%id_R1c,- rugB1 + (fB1RDc * self%R1R2X))
          _SET_ODE_(self%id_R2c,+ (fB1RDc * (1._rk - self%R1R2X)))
-#endif
 
          _SET_ODE_(self%id_O3c,+ fB1O3c/CMass)
          _SET_ODE_(self%id_O2o,- fB1O3c*self%urB1_O2X)
@@ -373,13 +288,6 @@ contains
 
 !..Source equations
 
-#ifdef DOCDYN
-         fRPB1p = sugB1*RPpP*self%sRPR1
-         _SET_ODE_(self%id_p, sum(fRPB1p))
-         do iRP=1,self%nRP
-            _SET_ODE_(self%id_RPp(iRP), - fRPB1p(iRP))
-         end do
-#endif
          _SET_ODE_(self%id_p, + fR1B1p - fB1N1p - fB1RDp)
          _SET_ODE_(self%id_N1p, + fB1N1p)
          _SET_ODE_(self%id_R1p, + fB1RDp - fR1B1p)
@@ -403,13 +311,6 @@ contains
 
 !..Source equations
 
-#ifdef DOCDYN
-         fRPB1n = sugB1*RPnP*self%sRPR1
-         _SET_ODE_(self%id_n, sum(fRPB1n))
-         do iRP=1,self%nRP
-            _SET_ODE_(self%id_RPn(iRP), - fRPB1n(iRP))
-         end do
-#endif
          _SET_ODE_(self%id_N4n, + fB1NIn)
          _SET_ODE_(self%id_n,   + fR1B1n - fB1NIn - fB1RDn)
          _SET_ODE_(self%id_R1n, + fB1RDn   - fR1B1n)
@@ -421,152 +322,134 @@ contains
 
    end subroutine do
 
-   
    subroutine remineralization(self,_ARGUMENTS_DO_)
 
       class (type_ersem_bacteria),intent(in) :: self
       _DECLARE_ARGUMENTS_DO_
-      
+
       integer :: iRP
-      real(rk) :: ETW,O2o
-      real(rk) :: etRemin
-#ifndef DOCDYN
       real(rk) :: R2cP
-      real(rk),dimension(self%nRP) :: RPc,RPn,RPp,RPcP,RPpP,RPnP
+      real(rk),dimension(self%nRP) :: RPc,RPn,RPcP,RPpP,RPnP
       real(rk),dimension(self%nRP) :: qnRPc,fRPr1
       real(rk) :: ruRPRD
-#endif
 #ifdef NOBAC
       real(rk) :: R1cP
       real(rk) :: fB1O3c
+      real(rk) :: ETW,etRemin
 #endif
-      real(rk) :: R1pP,R1nP,N4nP
+      real(rk) :: R1pP,R1nP
       real(rk) :: N7fP
       real(rk) :: fR1N1p,fR1NIn
       real(rk) :: sRPr1(self%nRP)
-      real(rk) :: RPfP(self%nRP),fRPN7f(self%nRP),n7fsink
+      real(rk) :: RPfP(self%nRP),n7fsink
 
       _LOOP_BEGIN_
 
-         _GET_(self%id_ETW,ETW)
-         _GET_(self%id_O2o,O2o)
+         ! -----------------------------------------
+         ! POM breakdown
+         ! -----------------------------------------
 
-#ifdef NOBAC
-         _GET_(self%id_R1c,R1cP)
-#endif
-         _GET_(self%id_R1p,R1pP)
-         _GET_(self%id_R1n,R1nP)
-
-         _GET_(self%id_N4n,N4nP)
-
-         etRemin = self%q10B1X**((ETW-10._rk)/10._rk) - self%q10B1X**((ETW-32._rk)/3._rk)
-
-!..fluxes from particulate to dissolved
-
-#ifdef DOCDYN
-         sRPr1 = self%sRPr1
-#else
-         _GET_(self%id_R2c,R2cP)
+         ! Get particulate organic matter concentrations. Also retrieve POC and PON including background,
+         ! so N:C can be computed without risk of division by 0.
          do iRP=1,self%nRP
-            _GET_WITH_BACKGROUND_(self%id_RPc(iRP),RPc(iRP))
-            _GET_WITH_BACKGROUND_(self%id_RPn(iRP),RPn(iRP))
-            _GET_WITH_BACKGROUND_(self%id_RPp(iRP),RPp(iRP))
             _GET_(self%id_RPc(iRP),RPcP(iRP))
             _GET_(self%id_RPp(iRP),RPpP(iRP))
             _GET_(self%id_RPn(iRP),RPnP(iRP))
+            _GET_WITH_BACKGROUND_(self%id_RPc(iRP),RPc(iRP))
+            _GET_WITH_BACKGROUND_(self%id_RPn(iRP),RPn(iRP))
          end do
+
+         ! Specific rate of particulate matter conversion (1/d) - substrate-specific and proportional to N:C
          qnRPc = RPn/RPc
-         sRPr1  = self%redfieldX*CMass * qnRPc * self%puRP_B1X 
-         fRPr1  = sRPr1 * RPcP
-         ruRPRD = sum(fRPr1)
-#ifdef NOBAC
-         ruRPRD = ruRPRD*etRemin
-#endif
-#ifdef SAVEFLX
-         fRPRDc(I) = ruRPRD
-#endif
-#endif
+         sRPr1  = self%redfieldX*CMass * qnRPc * self%puRP_B1X
 
 #ifdef NOBAC
-!..mineralisation of DOC to CO2
-
-         fB1O3c = self%sR1N1X * R1cP*etRemin
-          _SET_DIAGNOSTIC_(self%id_fB1O3c,fB1O3c)
-
-!..mineralisation of DOP to PO4
-
-         fR1N1p = self%sR1N1X * R1pP*etRemin
-
-!..mineralisation of DON to Nh4
-
-         fR1NIn = self%sR1N4X * R1nP*etRemin
-
-#else 
-!..mineralisation of DOP to PO4
-
-         fR1N1p = self%sR1N1X * R1pP
-
-!..mineralisation of DON to Nh4
-
-         fR1NIn = self%sR1N4X * R1nP
+         ! Apply temperature limitation factor to particulate organic matter conversion rates.
+         ! NB limitation factor is also used further down for mineralization of dissolved organic matter.
+         _GET_(self%id_ETW,ETW)
+         etRemin = self%q10B1X**((ETW-10._rk)/10._rk) - self%q10B1X**((ETW-32._rk)/3._rk)
+         sRPr1 = sRPr1*etRemin
 #endif
 
-    !..Source equations
-
-#ifdef DOCDYN
-         _SET_ODE_(self%id_R1p, - fR1N1p)
-         _SET_ODE_(self%id_R1n, - fR1NIn)
-#else
-#ifdef NOBAC
-         _SET_ODE_(self%id_R1C,  + ruRPRD - fB1O3c)
-         _SET_ODE_(self%id_O3c, + fB1O3c/CMass)
-         _SET_ODE_(self%id_O2o, - fB1O3c*self%urB1_O2X)
-#else
-         _SET_ODE_(self%id_R1C, + ruRPRD)
-#endif
+         ! Decrease in particulate organic matter due to conversion to dissolved organic matter.
          do iRP=1,self%nRP
-            _SET_ODE_(self%id_RPc(iRP), - fRPr1(iRP))
-            _SET_ODE_(self%id_RPp(iRP), - sRPr1(iRP)*RPpP(iRP))
-            _SET_ODE_(self%id_RPn(iRP), - sRPr1(iRP)*RPnP(iRP))
+            _SET_ODE_(self%id_RPc(iRP), -sRPr1(iRP)*RPcP(iRP))
+            _SET_ODE_(self%id_RPp(iRP), -sRPr1(iRP)*RPpP(iRP))
+            _SET_ODE_(self%id_RPn(iRP), -sRPr1(iRP)*RPnP(iRP))
          end do
-         _SET_ODE_(self%id_R1p, + sum(sRPr1*RPpP) - fR1N1p)
-         _SET_ODE_(self%id_R1n, + sum(sRPr1*RPnP) - fR1NIn)
-#endif
 
-         _SET_ODE_(self%id_N1p, + fR1N1p)
-         _SET_ODE_(self%id_N4n, + fR1NIn)
+         ! Increase in dissolved organic matter due to conversion from particulate organic matter.
+         _SET_ODE_(self%id_R1c, + sum(sRPr1*RPcP))
+         _SET_ODE_(self%id_R1p, + sum(sRPr1*RPpP))
+         _SET_ODE_(self%id_R1n, + sum(sRPr1*RPnP))
 
+         ! Conversion of particulate iron to inorganic Fe
          if (use_iron) then
-            ! remineralization of particulate iron to Fe
             do iRP=1,self%nRP
                _GET_(self%id_RPf(iRP),RPfP(iRP))
+               if (RPfP(iRP)/=0.0_rk) _SET_ODE_(self%id_RPf(iRP),-sRPr1(iRP)*RPfP(iRP))
             end do
-            fRPN7f=sRPr1*RPfP
+            _SET_ODE_(self%id_N7f, + sum(sRPr1*RPfP))
+         end if
 
-            ! sink of Fe
+         ! -----------------------------------------
+         ! DOM mineralization
+         ! -----------------------------------------
+         
+#ifdef NOBAC
+         ! Mineralisation of dissolved organic carbon to CO2
+         _GET_(self%id_R1c,R1cP)
+         fB1O3c = self%sR1N1X * R1cP*etRemin
+         _SET_ODE_(self%id_R1C, - fB1O3c)
+         _SET_ODE_(self%id_O3c, + fB1O3c/CMass)
+         _SET_ODE_(self%id_O2o, - fB1O3c*self%urB1_O2X)
+         _SET_DIAGNOSTIC_(self%id_fB1O3c,fB1O3c)
+#endif
 
+         ! Mineralisation of dissolved organic phosphorus to PO4
+         _GET_(self%id_R1p,R1pP)
+         fR1N1p = self%sR1N1X * R1pP
+
+         ! Mineralisation of dissolved organic nitrogen to NH4
+         _GET_(self%id_R1n,R1nP)
+         fR1NIn = self%sR1N4X * R1nP
+
+#ifdef NOBAC
+         ! Apply temperature limitation factor to DOP, DON mineralization.
+         fR1N1p = fR1N1p*etRemin
+         fR1NIn = fR1NIn*etRemin
+#endif
+
+         ! Fluxes due to mineralization of DOP and DON to PO4 and NH4.
+         _SET_ODE_(self%id_R1p, - fR1N1p)
+         _SET_ODE_(self%id_N1p, + fR1N1p)
+         _SET_ODE_(self%id_R1n, - fR1NIn)
+         _SET_ODE_(self%id_N4n, + fR1NIn)
+
+         ! -----------------------------------------
+         ! Iron scavenging 
+         ! -----------------------------------------
+
+         if (use_iron) then
             ! This term takes into account the scavenging due to hydroxide precipitation and it is supposed to be 
             ! regulated by a threshold concentration (0.6 nM). See Aumont et al., 2003 (GBC) and Vichi et al., 2007 (JMS) for references.
             ! (Luca, 12/08)
             _GET_(self%id_N7f,N7fP)
-            n7fsink=self%fsinkX*max(0._rk,N7fP-0.6_rk)
-            
-            do iRP=1,self%nRP
-               if (fRPn7f(iRP)/=0.0_rk) _SET_ODE_(self%id_RPf(iRP),-fRPn7f(iRP))
-            end do
-            _SET_ODE_(self%id_N7f,+sum(fRPn7f)-n7fsink)
+            n7fsink = self%fsinkX*max(0._rk,N7fP-0.6_rk)
+            _SET_ODE_(self%id_N7f,-n7fsink)
          end if
 
-!..Breakdown of semi labile to labile DOM
+         ! -----------------------------------------
+         ! Conversion from semi-labile to labile DOM 
+         ! -----------------------------------------
 
-#ifndef DOCDYN
+         _GET_(self%id_R2c,R2cP)
          _SET_ODE_(self%id_R1c, + self%rR2R1X * R2cP)
          _SET_ODE_(self%id_R2c, - self%rR2R1X * R2cP)
-#endif
 
       ! Leave spatial loops (if any)
       _LOOP_END_
 
    end subroutine remineralization
-   
+
 end module
