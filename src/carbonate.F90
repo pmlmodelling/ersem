a95b855e12d8c324ca1c3ffee5f03ab247cf5d03
Momme Butenschoen <momm@pml.ac.uk>
2015-07-17 09:14:09 +0100

    Introduce minimum limit for alaklinity for fully prognostic case.

--- src/carbonate.F90
+++ src/carbonate.F90
@@ -52,7 +52,7 @@ contains
 
       if (self%iswtalk==5) then
          ! Total alkalinity is a state variable.
-         call self%register_state_variable(self%id_TA,'TA','umol/kg','total alkalinity',minimum=0._rk, &
+         call self%register_state_variable(self%id_TA,'TA','umol/kg','total alkalinity',minimum=1.e-4_rk, &
             standard_variable=standard_variables%alkalinity_expressed_as_mole_equivalent)
       else
          ! Total alkalinity is a diagnostic variable, parameterized as function of salinity and temperature.
