73a810a7f0f10e1e50c8d563695fdaa4d4ad7d93
Jorn Bruggeman <jbr@pml.ac.uk>
2016-02-22 14:53:39 +0000

    corrected iopABS, iopBBS units in primary_producer - Wallhead

--- src/primary_producer.F90
+++ src/primary_producer.F90
@@ -132,8 +132,8 @@ contains
          call self%get_parameter(self%qurf,'qurf','m^3/mg C/d',  'specific affinity for iron')
       end if
       call self%get_parameter(EPS,     'EPS',    'm^2/mg C','specific shortwave attenuation', default=4.E-4_rk)
-      call self%get_parameter(iopABS,  'iopABS', 'm^2/mg C','specific shortwave absorption', default=8.E-3_rk)
-      call self%get_parameter(iopBBS,  'iopBBS', 'm^2/mg C','specific shortwave backscatter', default=3.E-3_rk)
+      call self%get_parameter(iopABS,  'iopABS', 'm^2/mg Chl','specific shortwave absorption', default=8.E-3_rk)
+      call self%get_parameter(iopBBS,  'iopBBS', 'm^2/mg Chl','specific shortwave backscatter', default=3.E-3_rk)
       call self%get_parameter(c0,          'c0',     'mg C/m^3','background carbon concentration', default=0.0_rk)
       call self%get_parameter(self%calcify,'calcify','',        'calcify',                         default=.false.)
       call self%get_parameter(self%rm,     'rm',     'm/d',     'background sinking velocity',     default=0.0_rk)
