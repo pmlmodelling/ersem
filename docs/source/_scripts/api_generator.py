900d3b44ba07fbb2196dca56ec6c6a27804fe10d
Michael Wathen <mwathen@gmail.com>
2021-02-02 14:04:09 +0000

    Updating api

--- docs/source/_scripts/api_generator.py
+++ docs/source/_scripts/api_generator.py
@@ -1,7 +1,9 @@
 from numpy.f2py.crackfortran import crackfortran
+from numpy import where
 import glob
 from contextlib import redirect_stdout
 import io
+import re
 
 
 def create(file_name):
@@ -16,29 +18,53 @@ def create(file_name):
     api_str = ".. _api:\n\n{}\n{}\n{}\n\n".format(heading_boarder,
                                                       main_header,
                                                       heading_boarder)
+
+    structure = ["include", "!", "module", "use"]
     f = io.StringIO()
     # removing standard output from console
     with redirect_stdout(f):
         for fname in glob.glob("../../src/*.F90"):
             if fname != "../../src/shared.F90":
+                with open(fname, "r") as f:
+                    file_contence = f.readlines()
+                module_dict = {"include": [], "!": [], "module": [], "use": []}
+                for line in file_contence:
+                    check = [s in line for s in structure]
+                    index = where(check)[0]
+                    if len(index) > 0:
+                        key = structure[index[0]]
+                        module_dict[key].append(line.split(key)[1][1:].strip("\n"))
+                    if "   private\n" in line:
+                        break
+                module_dict["class inheritance"] = module_dict.pop("use")
+                module_dict["description"] = module_dict.pop("!")
+                module_str = "Module name: {}\n{}\n\n".format(
+                        module_dict["module"][0],
+                        (len(module_dict["module"][0]) + 13) * "~")
+                module_dict.pop('module', None)
+                indent = " " * 4
+                for key, item in module_dict.items():
+                    if key == "description":
+                        list_str = \
+                                "\n".join(item).replace("\n", "\n" + indent * 2)
+                    else:
+                        list_str = f"\n{2*indent}.. code-block:: bash \n\n{indent*4}" + \
+                                "\n".join(item).replace("\n", "\n" + indent * 4)
+                    module_str += f"{indent}**{key.capitalize()}**:\n{indent*2}{list_str}\n\n"
+
+                # Find additional functions within module
                 interface = [sub["saved_interface"].split("\n")[1:-1]
                              for sub in crackfortran(fname)[0]["body"]
-                             if sub["name"] != "unknown_type" and sub["block"] in
-                             ["subroutine", "function"]]
-                subroutines = []
-                for i in interface:
-                    joined_str = "\n".join(i)
-                    multi_subroutine = joined_str.find("register_bn")
-                    if multi_subroutine == -1:
+                             if sub["block"] in ["function"]]
+                if interface != []:
+                    subroutines = []
+                    for i in interface:
+                        joined_str = "\n".join(i)
                         subroutines.append(".. code-block:: bash \n\n" + joined_str)
-                    else:
-                        for string in [joined_str[:multi_subroutine - 15],
-                                       joined_str[multi_subroutine - 15:-30]]:
-                            subroutines.append(".. code-block:: bash \n\n" + string)
-                heading = "{} module with the following " \
-                              "subroutines".format(fname.split("/")[-1].replace(".F90", ""))
-                boarder = "~" * len(heading)
-                api_str += "\n\n{}\n{}\n\n{}".format(heading, boarder, "\n".join(subroutines))
-    api_str = api_str.replace("do_bn", "do")
+                        func_str = "{}**Functions**:\n\n{}".format(indent,
+                                                           "\n".join(subroutines))
+                        module_str += func_str.replace("\n", "\n" + indent * 2) +"\n"
+            api_str += module_str
+
     with open(file_name, "w") as api_file:
         api_file.write(api_str)
