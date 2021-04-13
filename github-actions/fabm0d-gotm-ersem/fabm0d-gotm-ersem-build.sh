66e89dd2c7410ec2531825cae875925a98a3ba1c
Michael Wathen <mwathen@gmail.com>
2021-04-13 15:22:00 +0100

    Fixing GOTM version

--- github-actions/fabm0d-gotm-ersem/fabm0d-gotm-ersem-build.sh
+++ github-actions/fabm0d-gotm-ersem/fabm0d-gotm-ersem-build.sh
@@ -9,7 +9,7 @@ git clone https://github.com/fabm-model/fabm.git
 
 echo "Cloning GOTM"
 git clone https://github.com/gotm-model/code.git gotm
-cd gotm && git submodule update --init --recursive && cd ..
+cd gotm && git checkout v6.0 && git submodule update --init --recursive && cd ..
 
 echo "Checking out branch: $BRANCH"
 cd ersem && git checkout $BRANCH && cd ..
