90554c2929a3d3b0a245431f7bc80c439869dd0c
Michael Wathen <mwathen@gmail.com>
2021-10-21 11:10:46 +0100

    Updating timing tests to be a percentage of runtime

--- github-actions/gotm-fabm-ersem/test_profiling.py
+++ github-actions/gotm-fabm-ersem/test_profiling.py
@@ -15,12 +15,12 @@ def load_profile(csv_file):
     df = pd.read_csv(csv_file, header=None)
     row0 = df.loc[0].values.tolist()
     row1 = df.loc[1].values.tolist()
-    cols = ["{} {}".format(r0, r1).strip("nan") \
+    cols = ["{} {}".format(r0, r1).strip("nan")
             for r0, r1 in zip(row0, row1)]
-    cols = [r.strip(" ") if r.startswith(" ") or r.endswith(" ") else r \
+    cols = [r.strip(" ") if r.startswith(" ") or r.endswith(" ") else r
             for r in cols]
     df.columns = cols
-    df = df.drop(df.index[[0,1]])
+    df = df.drop(df.index[[0, 1]])
     df = df.dropna(axis='rows')
     for c in cols[:-1]:
         df[c] = df[c].astype(float)
@@ -29,6 +29,7 @@ def load_profile(csv_file):
     fabm = df.loc[df["name"].str.startswith("__fabm", na=False)]
     netcdf = df.loc[df["name"].str.startswith("__netcdf", na=False)]
     return df, ersem, gotm, fabm, netcdf
+
 
 def average_df(csv_path):
     csv_files = glob.glob(os.path.join(csv_path, "*.csv"))
@@ -65,50 +66,51 @@ class ProfillingTest(unittest.TestCase):
         profile_path = os.path.join("ersem-setups",
                                     "L4")
         expected_value_file = os.path.join(dir_path, "gprof.csv")
-        _, self.ersem_expected, self.gotm_expected, self.fabm_expected, \
-                self.netcdf_expected, = load_profile(expected_value_file)
-        _, self.ersem, self.gotm, self.fabm, self.netcdf, = \
-                average_df(profile_path)
+        self.df_expected, self.ersem_expected, self.gotm_expected, self.fabm_expected, \
+            self.netcdf_expected, = load_profile(expected_value_file)
+        self.df, self.ersem, self.gotm, self.fabm, self.netcdf, = \
+            average_df(profile_path)
 
-    def test_ersem_total_time(self):
+    def test_ersem_time_percentage(self):
         """
         Checks total ERSEM call times are the same
         """
-        self.ersem['self seconds'] = self.ersem['self seconds'].astype(float)
-        self.ersem_expected['self seconds'] = self.ersem_expected['self seconds'].astype(float)
-        total = self.ersem["self seconds"].sum()
-        total_expected = self.ersem_expected["self seconds"].sum()
-        assert abs((total - total_expected) / float(total) ) <= 0.05
+        ersem_perc = self.ersem["% time"].sum()
+        ersem_perc_expected = self.ersem_expected["% time"].sum()
+        assert abs(ersem_perc - ersem_perc_expected) <= 1.0
 
-    def test_gotm_total_time(self):
-        """
-        Checks total GOTM call times are the same
-        """
-        self.gotm['self seconds'] = self.gotm['self seconds'].astype(float)
-        self.gotm_expected['self seconds'] = self.gotm_expected['self seconds'].astype(float)
-        total = self.gotm["self seconds"].sum()
-        total_expected = self.gotm_expected["self seconds"].sum()
-        assert abs((total - total_expected) / float(total) ) <= 0.05
-
-    def test_fabm_total_time(self):
+    def test_fabm_time_percentage(self):
         """
         Checks total FABM call times are the same
         """
-        self.fabm['self seconds'] = self.fabm['self seconds'].astype(float)
-        self.fabm_expected['self seconds'] = self.fabm_expected['self seconds'].astype(float)
-        total = self.fabm["self seconds"].sum()
-        total_expected = self.fabm_expected["self seconds"].sum()
-        assert abs((total - total_expected) / float(total) ) <= 0.05
+        fabm_perc = self.fabm["% time"].sum()
+        fabm_perc_expected = self.fabm_expected["% time"].sum()
+        assert abs(fabm_perc - fabm_perc_expected) <= 1.0
 
-    def test_netcdf_total_time(self):
+    def test_gotm_time_percentage(self):
         """
-        Checks total NETCDF call times are the same
+        Checks total GOTM call times are the same
         """
-        self.netcdf['self seconds'] = self.netcdf['self seconds'].astype(float)
-        self.netcdf_expected['self seconds'] = self.netcdf_expected['self seconds'].astype(float)
-        total = self.netcdf["self seconds"].sum()
-        total_expected = self.netcdf_expected["self seconds"].sum()
-        assert abs((total - total_expected) / float(total) ) <= 0.05
+        gotm_perc = self.gotm["% time"].sum()
+        gotm_perc_expected = self.gotm_expected["% time"].sum()
+        assert abs(gotm_perc - gotm_perc_expected) <= 1.0
+
+    def test_netcdf_time_percentage(self):
+        """
+        Checks total netCDF call times are the same
+        """
+        netcdf_perc = self.netcdf["% time"].sum()
+        netcdf_perc_expected = self.netcdf_expected["% time"].sum()
+        assert abs(netcdf_perc - netcdf_perc_expected) <= 1.0
+
+    def test_total_time(self):
+        """
+        Checks to see that the total time does not change by more than
+        5%
+        """
+        total = self.df["self seconds"].sum()
+        total_expected = self.df_expected["self seconds"].sum()
+        assert abs((total_expected - total) / total_expected) <= .05
 
     def test_ersem_func_names(self):
         """
@@ -147,11 +149,14 @@ class ProfillingTest(unittest.TestCase):
         Check the number of func calls for ERSEM modules stays the same
         """
         self.ersem['self calls'] = self.ersem['self calls'].astype(float)
-        self.ersem_expected['self calls'] = self.ersem_expected['self calls'].astype(float)
+        self.ersem_expected['self calls'] = self.ersem_expected['self calls'].astype(
+            float)
         calls = \
-                {n: c for n, c in zip(self.ersem["name"], self.ersem['self calls'])}
+            {n: c for n, c in zip(
+                self.ersem["name"], self.ersem['self calls'])}
         calls_expected = \
-                {n: c for n, c in zip(self.ersem["name"], self.ersem['self calls'])}
+            {n: c for n, c in zip(
+                self.ersem["name"], self.ersem['self calls'])}
         assert calls == calls_expected
 
     def test_gotm_func_calls(self):
@@ -159,11 +164,12 @@ class ProfillingTest(unittest.TestCase):
         Check the number of func calls for GOTM modules stays the same
         """
         self.gotm['self calls'] = self.gotm['self calls'].astype(float)
-        self.gotm_expected['self calls'] = self.gotm_expected['self calls'].astype(float)
+        self.gotm_expected['self calls'] = self.gotm_expected['self calls'].astype(
+            float)
         calls = \
-                {n: c for n, c in zip(self.gotm["name"], self.gotm['self calls'])}
+            {n: c for n, c in zip(self.gotm["name"], self.gotm['self calls'])}
         calls_expected = \
-                {n: c for n, c in zip(self.gotm["name"], self.gotm['self calls'])}
+            {n: c for n, c in zip(self.gotm["name"], self.gotm['self calls'])}
         assert calls == calls_expected
 
     def test_fabm_func_calls(self):
@@ -171,11 +177,12 @@ class ProfillingTest(unittest.TestCase):
         Check the number of func calls for FABM modules stays the same
         """
         self.fabm['self calls'] = self.fabm['self calls'].astype(float)
-        self.fabm_expected['self calls'] = self.fabm_expected['self calls'].astype(float)
+        self.fabm_expected['self calls'] = self.fabm_expected['self calls'].astype(
+            float)
         calls = \
-                {n: c for n, c in zip(self.fabm["name"], self.fabm['self calls'])}
+            {n: c for n, c in zip(self.fabm["name"], self.fabm['self calls'])}
         calls_expected = \
-                {n: c for n, c in zip(self.fabm["name"], self.fabm['self calls'])}
+            {n: c for n, c in zip(self.fabm["name"], self.fabm['self calls'])}
         assert calls == calls_expected
 
     def test_netcdf_func_calls(self):
@@ -183,12 +190,12 @@ class ProfillingTest(unittest.TestCase):
         Check the number of func calls for NETCDF modules stays the same
         """
         self.netcdf['self calls'] = self.netcdf['self calls'].astype(float)
-        self.netcdf_expected['self calls'] = self.netcdf_expected['self calls'].astype(float)
+        self.netcdf_expected['self calls'] = self.netcdf_expected['self calls'].astype(
+            float)
         calls = \
-                {n: c for n, c in zip(self.netcdf["name"], self.netcdf['self calls'])}
+            {n: c for n, c in zip(
+                self.netcdf["name"], self.netcdf['self calls'])}
         calls_expected = \
-                {n: c for n, c in zip(self.netcdf["name"], self.netcdf['self calls'])}
+            {n: c for n, c in zip(
+                self.netcdf["name"], self.netcdf['self calls'])}
         assert calls == calls_expected
-
-
-
