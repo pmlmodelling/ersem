6bf542b52c47eefba63ed7488f399d840b7fbe24
Gennadi Lessin <gle@pml.ac.uk>
2022-07-27 11:58:58 +0100

    rearranged and cleaned

--- src/nitrous_oxide.F90
+++ src/nitrous_oxide.F90
@@ -23,10 +23,9 @@ module ersem_nitrous_oxide
    type,extends(type_ersem_pelagic_base),public :: type_ersem_nitrous_oxide
       ! Variables
       type (type_dependency_id)                     :: id_ETW,id_X1X
-      type (type_horizontal_dependency_id)          :: id_wnd,id_pN2Oa,id_fri
+      type (type_horizontal_dependency_id)          :: id_wnd,id_pN2Oa
       type (type_horizontal_diagnostic_variable_id) :: id_airsea
       type (type_diagnostic_variable_id)            :: id_satp
-      type (type_horizontal_diagnostic_variable_id) :: id_ice
 
       integer :: iswN2O
 
@@ -58,11 +57,9 @@ contains
       call self%register_dependency(self%id_X1X,standard_variables%practical_salinity)
       call self%register_dependency(self%id_wnd,standard_variables%wind_speed)
       call self%register_dependency(self%id_pN2Oa,partial_pressure_of_n2o)
-      call self%register_dependency(self%id_fri,ice_fraction)  
       call self%register_diagnostic_variable(self%id_satp,'satp','%','nitrous oxide % saturation')
       call self%register_diagnostic_variable(self%id_airsea,'airsea','mmol N2O/m^2/d','airsea flux of N2O',source=source_do_surface)
       call self%get_parameter(self%iswN2O,'iswN2O','','air-sea flux switch (0: off, 1: on)',default=1)
-      call self%register_diagnostic_variable(self%id_ice,'ice','%','ice fraction',source=source_do_surface)
 
    end subroutine initialize
 
@@ -70,31 +67,16 @@ contains
       class (type_ersem_nitrous_oxide), intent(in) :: self
       _DECLARE_ARGUMENTS_DO_
 
-      real(rk) :: O5n,ETW,X1X,wnd, fwind,TK,TK100
-      real(rk) :: koN2O,sc,n2oflux,pN2Oa
-      real(rk),parameter :: A1 = -62.7062_rk
-      real(rk),parameter :: A2 = 97.3066_rk
-      real(rk),parameter :: A3 = 24.1406_rk
-      real(rk),parameter :: B1 = -0.05842_rk
-      real(rk),parameter :: B2 = 0.033193_rk
-      real(rk),parameter :: B3 = -0.0051_rk
+      real(rk) :: O5n,ETW,X1X
+      real(rk) :: koN2O,pN2Oa
 
        _LOOP_BEGIN_
           _GET_(self%id_n,O5n)
           _GET_(self%id_ETW,ETW)
           _GET_(self%id_X1X,X1X)
-          _GET_HORIZONTAL_(self%id_wnd,wnd)
           _GET_HORIZONTAL_(self%id_pN2Oa,pN2Oa)
 
-        TK=ETW+273.15_rk
-        TK100=TK/100._rk
-
-        sc=2301._rk-151._rk*ETW+4.74_rk*ETW**2._rk-0.06_rk*ETW**3
-        fwind =  0.39_rk * wnd**2 *(sc/660._rk)**(-0.5_rk)
-        fwind=fwind*24._rk/100._rk   ! convert to m/day
-
-        koN2O = exp(A1+A2/tk100 + A3 * log(tk100) + &
-        &       X1X * (B1 + B2 * tk100 + B3 * tk100 ** 2._rk))
+         koN2O = n2o_transfer_coefficient(self,ETW,X1X)
 
        _SET_DIAGNOSTIC_(self%id_satp,(100._rk*O5n*0.5_rk/(KoN2O*pN2Oa*1.e-9*1.e6)))
       _LOOP_END_
@@ -104,14 +86,8 @@ contains
       class (type_ersem_nitrous_oxide), intent(in) :: self
       _DECLARE_ARGUMENTS_DO_SURFACE_
 
-      real(rk) :: O5n,ETW,X1X,wnd, fwind,TK,TK100, fri
+      real(rk) :: O5n,ETW,X1X,wnd, fwind
       real(rk) :: koN2O,sc,n2oflux,pN2Oa
-      real(rk),parameter :: A1 = -62.7062_rk
-      real(rk),parameter :: A2 = 97.3066_rk
-      real(rk),parameter :: A3 = 24.1406_rk
-      real(rk),parameter :: B1 = -0.05842_rk
-      real(rk),parameter :: B2 = 0.033193_rk
-      real(rk),parameter :: B3 = -0.0051_rk
 
       _HORIZONTAL_LOOP_BEGIN_
          _GET_(self%id_n,O5n)
@@ -119,23 +95,14 @@ contains
          _GET_(self%id_X1X,X1X)
          _GET_HORIZONTAL_(self%id_wnd,wnd)
          _GET_HORIZONTAL_(self%id_pN2Oa,pN2Oa)
-         _GET_HORIZONTAL_(self%id_fri,fri)
-         _SET_HORIZONTAL_DIAGNOSTIC_(self%id_ice,fri)
 
-!         OSAT = oxygen_saturation_concentration(self,ETW,X1X)
 ! Schmidt number for N2O (Wanninkhof, 1992)
-  
-        TK=ETW+273.15_rk
-        TK100=TK/100._rk
  
         sc=2301._rk-151._rk*ETW+4.74_rk*ETW**2._rk-0.06_rk*ETW**3
         fwind =  0.39_rk * wnd**2 *(sc/660._rk)**(-0.5_rk)
         fwind=fwind*24._rk/100._rk   ! convert to m/day
 
-
-        
-        koN2O = exp(A1+A2/tk100 + A3 * log(tk100) + &
-        &       X1X * (B1 + B2 * tk100 + B3 * tk100 ** 2._rk))
+        koN2O = n2o_transfer_coefficient(self,ETW,X1X)
 
 !        PNOatm is given in natm, so it is here converted to atm  by means of a 1.-9 factor. As KoNO is
 !        given in mol L-1 atm -1 (Weiss and Price, 1980), a conversion factor of
@@ -143,16 +110,10 @@ contains
 !        of N so it is multiplied by 0.5 to have mmol of N2O. (Luca, July 2016)
 
         if (self%iswN2O .eq. 1) then
-         if (fri .eq. 0._rk) then
-         n2oflux=(1._rk-fri) * fwind*(KoN2O*pN2Oa*1.e-9*1.e6-O5n*0.5_rk)
-         else
-         n2oflux=0._rk
-         endif
+         n2oflux = fwind*(KoN2O*pN2Oa*1.e-9*1.e6-O5n*0.5_rk)
         else
          n2oflux=0._rk
         endif
-
-!         UPTAKE=0.
 
          _SET_HORIZONTAL_DIAGNOSTIC_(self%id_airsea,n2oflux)
 
@@ -160,5 +121,24 @@ contains
 
       _HORIZONTAL_LOOP_END_
    end subroutine do_surface
+
+   function n2o_transfer_coefficient(self,ETW,X1X) result(koN2O)
+       class (type_ersem_nitrous_oxide), intent(in) :: self
+       real(rk),                         intent(in) :: ETW,X1X
+       real(rk)                                     :: koN2O
+       real(rk)           :: tk,tk100
+       real(rk),parameter :: A1 = -62.7062_rk
+       real(rk),parameter :: A2 = 97.3066_rk
+       real(rk),parameter :: A3 = 24.1406_rk
+       real(rk),parameter :: B1 = -0.05842_rk
+       real(rk),parameter :: B2 = 0.033193_rk
+       real(rk),parameter :: B3 = -0.0051_rk
+
+       TK=ETW+273.15_rk
+       TK100=TK/100._rk
+
+        koN2O = exp(A1+A2/tk100 + A3 * log(tk100) + &
+        &       X1X * (B1 + B2 * tk100 + B3 * tk100 ** 2._rk))
+    end function
    
 end module
