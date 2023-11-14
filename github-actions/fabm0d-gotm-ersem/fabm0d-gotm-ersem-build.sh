fa3413ae2ab0c72adab9bce48f28fc154b739f15
Michael Wathen <mwathen@gmail.com>
2023-11-14 12:09:31 +0000

    Updating gotm version in tests

--- github-actions/fabm0d-gotm-ersem/fabm0d-gotm-ersem-build.sh
+++ github-actions/fabm0d-gotm-ersem/fabm0d-gotm-ersem-build.sh
@@ -18,8 +18,7 @@ echo "Cloning FABM"
 git clone https://github.com/fabm-model/fabm.git
 
 echo "Cloning GOTM"
-git clone https://github.com/gotm-model/code.git gotm
-cd gotm && git checkout v6.0 && git submodule update --init --recursive && cd ..
+git clone --recursive https://github.com/gotm-model/code.git gotm
 
 echo "Checking out branch: $BRANCH"
 cd ersem && git checkout $BRANCH && cd ..
