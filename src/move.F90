fbd302d79380203b36752e091a43ba1ff8abc60d
Rebecca Millington <cobalt93@googlemail.com>
2025-06-12 15:17:58 +0100

    Adding diagnostic for overwintering so that mesozoo do not vertically migrate while overwintering

--- src/move.F90
+++ src/move.F90
@@ -17,6 +17,7 @@ type, extends(type_base_model) :: type_move
     type (type_state_variable_id)      :: id_target
     type (type_bottom_dependency_id)   :: id_integral
     type (type_bottom_dependency_id)   :: id_integral_random_weights
+    type (type_bottom_dependency_id)   :: id_overwintering
     real(rk) :: tstep, m, ratioMig
     contains
         procedure :: initialize
@@ -35,6 +36,7 @@ contains
         call self%register_dependency(self%id_integral,'integral','','depth-integrated target variable')
         call self%register_dependency(self%id_integral_random_weights,'migrator_integral_random_weights','','migrators distribution integral random weights')
         call self%register_dependency(self%id_thickness, standard_variables%cell_thickness)
+        call self%register_dependency(self%id_overwintering, 'overwintering','','Is mesozooplankton in diapause?')
         call self%get_parameter( self%ratioMig,  'ratioMig',    '-',      'ratio of moving biomass', default=0.5_rk)
         call self%get_parameter( self%tstep,  'tstep',    'sec',      'time-step in seconds', default=900.0_rk)
 
@@ -45,19 +47,22 @@ contains
        _DECLARE_ARGUMENTS_DO_
 
         real(rk) :: integral_random_weights, random_weights, integral
-        real(rk) :: local, distributed, thickness, target0
+        real(rk) :: local, distributed, thickness, overwintering
         
         _LOOP_BEGIN_
            _GET_BOTTOM_(self%id_integral,integral)
            _GET_BOTTOM_(self%id_integral_random_weights,integral_random_weights)         
+           _GET_BOTTOM_(self%id_overwintering,overwintering)
            _GET_(self%id_target,local)
            _GET_(self%id_random_weights,random_weights)
            _GET_(self%id_thickness,thickness)
-    
+
+           if (overwintering < 1.0_rk) then
            distributed = random_weights / integral_random_weights ! this ensures that total weight distribution is 1, so mass conserved
            !_SET_(self%id_target, max(0.0_rk,local * (1.0_rk - self%ratioMig) + (distributed * target0/max(thickness,1.0E-20_rk)) * self%ratioMig ) )
            _ADD_SOURCE_(self%id_target, (distributed * integral - local) * self%ratioMig / self%tstep)
            !_ADD_SOURCE_(self%id_target,  max(-max(local,0.0_rk),(distributed * integral /max(thickness,1.0E-20_rk) - max(local,0.0_rk)) * self%ratioMig) / self%tstep)
+           end if
         _LOOP_END_
         
     end subroutine do
