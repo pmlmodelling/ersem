1abbf5936c67ffdef944144a18d2b7c139c3600c
Rebecca Millington <rmi@scyllalg01.head.cm.hpcm.npm.ac.uk>
2025-06-20 14:07:37 +0100

    tidying code

--- src/weight_distribution.F90
+++ src/weight_distribution.F90
@@ -11,15 +11,13 @@ private
 
 type, extends(type_base_model), public :: type_weight_distribution
     type (type_bottom_diagnostic_variable_id) :: id_integral
-    !type (type_bottom_diagnostic_variable_id) :: id_totalthk
-    type (type_bottom_diagnostic_variable_id) :: id_integral_random_weights
-    type (type_diagnostic_variable_id)        :: id_random_weights
+    type (type_bottom_diagnostic_variable_id) :: id_integral_weights
+    type (type_diagnostic_variable_id)        :: id_weights
     type (type_state_variable_id)             :: id_target
     type (type_dependency_id)                 :: id_present
     type (type_dependency_id)                 :: id_migrator_food
-    !type (type_horizontal_dependency_id)            :: id_migrator_food0
     type (type_dependency_id)                 :: id_thickness
-    type (type_surface_dependency_id)         :: id_par0 !, id_parmean0
+    type (type_surface_dependency_id)         :: id_par0
     type (type_bottom_dependency_id)          :: id_topo
     type (type_dependency_id)                 :: id_depth    
 
@@ -33,9 +31,9 @@ contains
     subroutine initialize(self, configunit)
         class (type_weight_distribution), intent(inout), target :: self
         integer,                     intent(in)            :: configunit
-        call self%register_diagnostic_variable(self%id_integral_random_weights,'migrator_integral_random_weights','-','migrators distribution integral random weights', missing_value=0.0_rk, &
+        call self%register_diagnostic_variable(self%id_integral_weights,'migrator_integral_weights','-','migrators distribution integral weights', missing_value=0.0_rk, &
             act_as_state_variable=.true., source=source_do_column)
-        call self%register_diagnostic_variable(self%id_random_weights,'migrator_random_weights','-','migrators distribution random weights', missing_value=0.0_rk, &
+        call self%register_diagnostic_variable(self%id_weights,'migrator_weights','-','migrators distribution weights', missing_value=0.0_rk, &
             act_as_state_variable=.true., source=source_do_column)
         call self%register_state_dependency(self%id_target, 'target', '', 'variable to depth-integrate')   
         call self%register_dependency(self%id_present,'present', '-', 'migrators are present here')
@@ -56,16 +54,15 @@ contains
         class (type_weight_distribution), intent(in) :: self
         _DECLARE_ARGUMENTS_DO_COLUMN_
 
-        real(rk) :: thickness, integral_random, present, local_random, minimum_value !local
+        real(rk) :: thickness, integral_weights, present, weights, minimum_value
         real(rk) :: depth,topo, depth_threshold
         real(rk) :: par0, local
-        real(rk) :: food !, food0
-        real(rk) :: integral !totalthk, 
+        real(rk) :: food 
+        real(rk) :: integral  
         real(rk) :: search_food
 
         integral = 0.0_rk
-        integral_random = 0.0_rk
-        !totalthk = 0.0_rk
+        integral_weights = 0.0_rk
         depth_threshold = 600.0_rk
 
 
@@ -76,16 +73,14 @@ contains
             _GET_(self%id_thickness,thickness)
             _GET_(self%id_migrator_food,food)
             _GET_(self%id_depth,depth)
-            !_GET_SURFACE_(self%id_migrator_food0,food0)
             _GET_SURFACE_(self%id_par0,par0)
             _GET_BOTTOM_(self%id_topo,topo)
 
             integral = integral + local*thickness
-            !totalthk = totalthk + thickness
-
 
             minimum_value = 0.002 
 
+            ! finalise this
             if (par0 <= 1.0_rk) then
                 ! night
                 search_food = food
@@ -95,28 +90,28 @@ contains
             end if
 
             thickness = max(thickness, 1.0E-20_rk)
+
             if (present > 0.5_rk) then
-                 local_random = (minimum_value + (1.0_rk - minimum_value) * search_food ) 
+                 weight = (minimum_value + (1.0_rk - minimum_value) * search_food ) 
             else
                 if (depth <= min(depth_threshold,topo)) then
-                     ! what does this condtion do?   
-                     !local_random = thickness * 0.002   
-                     local_random = (minimum_value  + (0.01_rk - minimum_value) * search_food )  
+                     ! add comment here   
+                     weight = (minimum_value  + (0.01_rk - minimum_value) * search_food )  
                 else
-                     !local_random = thickness * 0.002  
-                     local_random = (minimum_value + (0.01_rk - minimum_value) &
+                     weight = (minimum_value + (0.01_rk - minimum_value) &
                          * exp(-0.025_rk * (depth - min(depth_threshold, topo))) * search_food ) 
                 end if 
             end if
 
-            integral_random = integral_random + local_random*thickness
+            integral_weights = integral_weights + weight*thickness
             
-            _SET_DIAGNOSTIC_(self%id_random_weights,local_random)
+            _SET_DIAGNOSTIC_(self%id_weights,weight)
 
         _VERTICAL_LOOP_END_
+
         _SET_BOTTOM_DIAGNOSTIC_(self%id_integral,integral)
-        !_SET_BOTTOM_DIAGNOSTIC_(self%id_totalthk,totalthk)
-        _SET_BOTTOM_DIAGNOSTIC_(self%id_integral_random_weights,integral_random)
+        _SET_BOTTOM_DIAGNOSTIC_(self%id_integral_weights,integral_weights)
+
     end subroutine do_column
 
 end module
