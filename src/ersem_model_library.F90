f061011f0bf31112954f7bd991e7f6f17d9cdd76
Rebecca Millington <rmi@pml.ac.uk>
2025-07-14 16:25:59 +0100

    Update ersem_model_library.F90
    Remove get_dependencies from model library

--- src/ersem_model_library.F90
+++ src/ersem_model_library.F90
@@ -31,7 +31,6 @@ module ersem_model_library
    use ersem_fluff
    use ersem_zenith_angle
    use dvm_conservative_migrator
-   use dvm_get_dependencies
    use dvm_move
    use dvm_upper_lower_boundaries_simple
    use dvm_weight_distribution
@@ -89,7 +88,6 @@ contains
          case ('fluff');                                   allocate(type_ersem_fluff::model)
          case ('zenith_angle');                            allocate(type_ersem_zenith_angle::model)
          case ('dvm_conservative_migrator');               allocate(type_conservative_migrator::model)
-         case ('dvm_get_dependencies');                    allocate(type_get_dependencies::model)
          case ('dvm_move');                                allocate(type_move::model)
          case ('dvm_upper_lower_boundaries_simple');       allocate(type_upper_lower_boundaries_simple::model)
          case ('dvm_weight_distribution');                 allocate(type_weight_distribution::model)
