9e3ba27aad95148e0e5c2678a0d1484f4d4226f7
Gennadi Lessin <58948398+glessin@users.noreply.github.com>
2026-07-27 16:00:23 +0100

    fixed freox and fanox diags, added units, fixed reference (#178)
    * fixed freox and fanox diags, added units, fixed reference
    
    * added long names for freox and fanox

--- src/bacteria_docdyn.F90
+++ src/bacteria_docdyn.F90
@@ -43,7 +43,7 @@ module ersem_bacteria_docdyn
       real(rk) :: DeniX,reoX,omroX,omonX,chN3oX
       integer  :: denit
 
-      ! Remineralization
+      ! Remineralisation
       real(rk) :: sR1N1X,sR1N4X
       real(rk) :: fsinkX
    contains
@@ -98,12 +98,12 @@ contains
       call self%register_state_dependency(self%id_N3n,'N3n','mmol N/m^3','nitrate')
       call self%register_state_dependency(self%id_N6,'N6','mmol HS-/m^3','reduction equivalent')
 
-      call self%register_diagnostic_variable(self%id_fdenit,'fdenit','mmol N/m^3/d','denitrification', missing_value=0._rk)
-      call self%register_diagnostic_variable(self%id_fanox,'fanox',  '-',           'fanox',           missing_value=0._rk)
-      call self%register_diagnostic_variable(self%id_freox,'freox',  '-',           'freox',           missing_value=0._rk)
+      call self%register_diagnostic_variable(self%id_fdenit, 'fdenit', 'mmol N/m^3/d', 'denitrification', missing_value=0._rk)
+      call self%register_diagnostic_variable(self%id_fanox, 'fanox', 'mmol HS-/m^3/d', 'formation of reduction equivalents', missing_value=0._rk)
+      call self%register_diagnostic_variable(self%id_freox, 'freox','mmol HS-/m^3/d', 'reoxidation of reduction equivalents', missing_value=0._rk)
       end if
 
-      ! Remineralization parameters
+      ! Remineralisation parameters
       call self%get_parameter(self%sR1N1X,   'sR1N1',   '1/d',    'mineralisation rate of labile dissolved organic phosphorus')
       call self%get_parameter(self%sR1N4X,   'sR1N4',   '1/d',    'mineralisation rate of labile dissolved organic nitrogen')
       call self%get_parameter(self%fsinkX,   'fsink',   '1/d',    'scavenging rate for iron')
@@ -322,7 +322,7 @@ contains
          fB1R3c=self%frB1R3*rraB1
          fB1RDc = fB1R1c + fB1R2c + fB1R3c
 
-! Denitrification implemented as in Sankar et al. (2008), doi.org/10.1016/j.ecolmodel.2018.01.016
+! Denitrification implemented as in Sankar et al. (2018), doi.org/10.1016/j.ecolmodel.2018.01.016
      if (self%denit == 1) then      
         denitpot = self%DeniX * N3n !eq.6
         deniteff = max(0._rk, self%urB1_O2X * (1._rk-o2state) * fB1O3c / self%omonX) !eq.7
@@ -331,12 +331,12 @@ contains
         _SET_DIAGNOSTIC_(self%id_fdenit,fdenit)
         _SET_ODE_(self%id_N3n, -fdenit)
 
-! Reduced sulfur formation corresponds to eq.9 in Sankar et al. (2008)
+! Reduced sulfur formation corresponds to eq.9 in Sankar et al. (2018)
         fanox = self%omroX * (self%urB1_O2X * (1._rk-o2state) * fB1O3c - self%omonX * fdenit)
         freox = self%reoX * etB1 * o2state * N6
 
-        _SET_DIAGNOSTIC_(self%id_fanox,freox)
-        _SET_DIAGNOSTIC_(self%id_freox,fanox)
+        _SET_DIAGNOSTIC_(self%id_fanox,fanox)
+        _SET_DIAGNOSTIC_(self%id_freox,freox)
 
         _SET_ODE_(self%id_N6, fanox - freox)
      end if 
@@ -497,7 +497,7 @@ contains
          _SET_DIAGNOSTIC_(self%id_minp,fR1N1p)
 
          if (use_iron) then
-            ! remineralization of particulate iron to Fe
+            ! remineralisation of particulate iron to Fe
             do iRP=1,self%nRP
                _GET_(self%id_RPf(iRP),RPfP(iRP))
             end do
